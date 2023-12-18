bee8d7cd36d3b53150480a5201d9a49391623b28
Cromyx <35427266+Cromyyx@users.noreply.github.com>
2023-12-18 12:39:11 +0100

    Calculates the sum of an amount of dice with an abount of sides.

---
+++ Dice Sum Calc.py
@@ -0,0 +1,15 @@
+#Calculates the sum of an amount of dice with an abount of sides.
+import random
+
+
+def dice(amount, sides):
+    summ = 0
+    for i in range(amount):
+        number = random.randint(1, sides)
+        print("Würfel nr.", i + 1, number)
+        summ = summ + number
+    return summ
+
+
+summ = dice(int(input("Anzahl der Würfel ")), int(input("Anzahl der Würfelseiten ")))
+print("Summe Zahlen:", summ)
