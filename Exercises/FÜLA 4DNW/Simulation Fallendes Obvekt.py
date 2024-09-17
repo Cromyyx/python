82cfc75abf1bdfb089f61904716609cbebfaec86
Cromyx <35427266+Cromyyx@users.noreply.github.com>
2024-09-17 14:19:26 +0200

    Compound Interest

--- Exercises/FÜLA 4DNW/Simulation Fallendes Obvekt.py
+++ Exercises/FÜLA 4DNW/Simulation Fallendes Obvekt.py
@@ -1,4 +1,4 @@
-initial_prinzipal_balance =\
+initial_principal_balance =\
     float(input("Enter initial balance: "))
 interest_rate =\
     float(input("Enter interest rate in %: "))
@@ -7,5 +7,7 @@ number_of_times_interest_is_applied =\
 number_of_time_periods_elapsed =\
     float(input("number of time periods elapsed: "))
 
-final_amount = initial_prinzipal_balance * (1 + ((interest_rate / 100) / number_of_times_interest_is_applied)) ** (number_of_times_interest_is_applied * number_of_time_periods_elapsed)
+final_amount = (initial_principal_balance
+                * (1 + ((interest_rate / 100) / number_of_times_interest_is_applied))
+                ** (number_of_times_interest_is_applied * number_of_time_periods_elapsed))
 print(final_amount)
